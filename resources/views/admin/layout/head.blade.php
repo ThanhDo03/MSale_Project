2ef12857e21886d485709fa67af598e933472624
ThanhDo03 <tranthanhdo2407@gmail>
2023-03-24 08:51:09 +0700

    ThanhDo commit fix function change pwd

--- resources/views/admin/layout/head.blade.php
+++ resources/views/admin/layout/head.blade.php
@@ -9,6 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
+    <link rel="icon" type="image/png" href="{{ asset('image/Favicon/TTD.png') }}" style="border-radius:50px">
 
 
     <title>Admin</title>
